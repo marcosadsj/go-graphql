a09b39dd9d78b0fc7b3b07fb5b1a30dd129c0bf3
marcosadsj <marcosadsj@gmail.com>
2025-06-08 20:53:33 -0300

    chore: add courses table

--- tables.sql
+++ tables.sql
@@ -1,1 +1,13 @@
-CREATE TABLE categories (id string PRIMARY KEY, name varchar(255) NOT NULL, description varchar(255));
+CREATE TABLE categories (
+    id string PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    description varchar(255)
+);
+
+CREATE TABLE courses (
+    id string PRIMARY KEY,
+    title varchar(255) NOT NULL,
+    description varchar(255),
+    category_id string,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
